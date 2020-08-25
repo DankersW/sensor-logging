5ccb6cb673b1400b1e35ddd763f6a81f26e5acfd
Dankers Wouter <wouter.dankers@volvo.com>
2020-08-25 22:30:55 +0200

    Point of entry added

--- src/sensor_reader.py
+++ src/sensor_reader.py
@@ -1,20 +1,24 @@
 from src.mqtt_config import MqttConfig
 from src.decoder import Decoder
 
+from threading import Thread
 import subprocess
 import paho.mqtt.client as mqtt
 
 
-class SensorReader:
+class SensorReader(Thread):
     def __init__(self, simulator_command):
+        Thread.__init__(self)
         self.decoder = Decoder()
         self.running = False
         self.mqtt_client = self.setup_mqtt()
         self.process = subprocess.Popen(simulator_command, shell=False, stdout=subprocess.PIPE)
-        self.read_sensor()
 
     def __del__(self):
         self.mqtt_client.disconnect()
+
+    def run(self):
+        self.read_sensor()
 
     def setup_mqtt(self):
         client = mqtt.Client()
@@ -28,7 +32,6 @@ class SensorReader:
             package_length = int.from_bytes(bin_plength, byteorder='big')
             bin_package = self.read_binary_data(package_length - self.decoder.Sizes.plen)
             log_msg = self.decoder.decode_log_data(bin_package)
-            print('{} - {} | {} | {}'.format(bin_plength, bin_package, package_length, log_msg))
             self.mqtt_client.publish(topic=MqttConfig.telemetry_topic, payload=log_msg, qos=0)
 
     def read_binary_data(self, read_size):
@@ -36,11 +39,3 @@ class SensorReader:
             binary_data = self.process.stdout.read(read_size)
             if binary_data:
                 return binary_data
-
-
-if __name__ == '__main__':
-    cmd = './sensor_data_simulator.x86_64-unknown-linux-gnu'
-    arg = '--name=445t-e1'
-    command = [cmd]#, arg]
-
-    sensor_reader = SensorReader(command)
